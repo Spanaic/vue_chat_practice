7a41e73c2655d3b37d1c388ff4c4800f89452f92
Spanaic <spanaic@gmail.com>
2019-10-23 21:42:26 +0900

    chatコンポーネントを作成中

---
+++ src/components/Chat.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <div class="chat">
+      <div class="conversation">
+        <ul class="conversation_inner">
+          <li v-for="(list,i) in messageLists" :key="i">
+            <div class="conversation_user-image"><img src="list.profilePicUrl" alt=""></div>
+            <div class="conversation_col">
+              <div class="conversation_user-name"> {{list.name}} </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Chat from '@/components/Chat'
+
+Vue.use(Router)
+// moduleのindexを参照している可能性あり
+
+export default new Router ({
+    routes: [
+        // 配列の中にオブジェクトを設定して、パスと名前付きルート、呼び出すコンポーネントを指定する
+        {
+            path: '/',
+            name: 'Chat',
+            component: Chat
+        }
+    ]
+})
+
+// indexにRouter設定の記述も行っていく。
